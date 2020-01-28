404bc9592884278dc4d379997222b6e4c9ac67bc
Sammy Robens-Paradise <srobensparadise@gmail.com>
2020-01-27 21:04:30 -0500

    added tab icons

--- src/components/editor-wrapper.js
+++ src/components/editor-wrapper.js
@@ -1,8 +1,9 @@
 /* eslint-disable implicit-arrow-linebreak */
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react'
+import PropTypes from 'prop-types'
 import styled from 'styled-components'
-import { Icon, CodeBlock } from 'global-winery'
+import { Icon, CodeBlock, ExitIcon } from 'global-winery'
 import Editor from './App'
 import { fallbackConfig, isObjectEmpty } from '../utils/data-handler'
 
@@ -22,7 +23,7 @@ const Wrapper = styled.div`
 `
 const Tab = styled.div`
   width: ${(p) => (p.width ? p.width / p.numberOfTabs : 400 / p.numberOfTabs)}px;
-  height: ${(p) => (p.height ? p.height - 400 : 200)}px;
+  height: 40px;
   background: ${(p) => (p.color ? p.color : colorsLib.defaultVSCodeColor)};
   float: right;
   border-radius: 10px 10px 0px 0px;
@@ -31,32 +32,82 @@ const Tab = styled.div`
     - parseInt(p.width, 10) / parseInt(p.numberOfTabs, 10)}px;
   position: absolute;
   top: 0px;
+  box-shadow: 6px 0px 32px -15px rgba(0, 0, 0, 0.75);
+  z-index: ${(p) => 20 - p.order};
 `
 const E = styled.div`
+  height: 40px;
+  width: 100%;
+  margin-top: 40px;
+  background-color: ${colorsLib.defaultVSCodeColor};
   z-index: 1000;
 `
 const IconWrapper = styled.div`
-  transform: translateY(-30px) translateX(20px);
+  position: absolute;
+  top: 2px;
+  left: 6px;
   outline: none;
+  z-index: 999;
+  transform: scale(0.8);
 `
+const IconExitWrapper = styled.div`
+  position: absolute;
+  top: 2px;
+  right: 6px;
+  outline: none;
+  float: right;
+  z-index: 999;
+  transform: scale(0.8);
+`
+const ActiveFile = styled.h1`
+  color: #fff;
+  font-size: 12px;
+  text-align: center;
+  margin-block-start: 0px;
+  font-family: 'Montserrat', sans-serif;
+  font-weight: 200;
+`
+const FileName = styled.h1`
+  color: #fff;
+  font-size: 16px;
+  text-align: center;
+  margin-block-start: 0px;
+  position: absolute;
+  top: 11px;
+  left: 50px;
+  text-decoration: none;
+  font-style: normal;
+  font-family: 'Montserrat', sans-serif;
+  font-weight: 200;
+`
+const GetTabs = ({ numberOfTabs, width, fileNamesInEditor }) => {
+  const cb = <CodeBlock color="#fff" />
+  const exit = <ExitIcon color="#fff" />
 
-const GetTabs = ({ numberOfTabs, width }) => {
+  const {
+    firstFile, secondFile, thirdFile, fallback,
+  } = fileNamesInEditor
   const tabs = []
   let colorAsProps
+  let filenameAsProps
   // eslint-disable-next-line no-plusplus
   for (let i = 0; i < numberOfTabs; i++) {
     switch (i) {
       case 0:
         colorAsProps = colorsLib.efaultVSCodeColor
+        filenameAsProps = firstFile
         break
       case 1:
         colorAsProps = colorsLib.secondaryCodeColor
+        filenameAsProps = secondFile
         break
       case 2:
         colorAsProps = colorsLib.tertiaryVSCodeColor
+        filenameAsProps = thirdFile
         break
       default:
         colorAsProps = colorsLib.defaultVSCodeColor
+        filenameAsProps = fallback
         break
     }
     tabs.push(
@@ -66,7 +117,15 @@ const GetTabs = ({ numberOfTabs, width }) => {
         color={colorAsProps}
         numberOfTabs={numberOfTabs}
         width={width}
-      />,
+      >
+        <IconWrapper>
+          <Icon type={cb} />
+        </IconWrapper>
+        <FileName>{filenameAsProps}</FileName>
+        <IconExitWrapper>
+          <Icon type={exit} />
+        </IconExitWrapper>
+      </Tab>,
     )
   }
   return tabs.map(() => <div>{tabs}</div>)
@@ -82,22 +141,23 @@ class EditorWrapper extends React.PureComponent {
     this.state = {
       localConfig: retrievedConfigFromLocalStorage,
       numberOfTabs: 3,
+      activeFileName: props.activeFileName,
+      fileNamesInEditor: props.fileNamesInEditor,
     }
     window.Editor_dispatchDataEventToLocalStorage('config', fallbackConfig)
   }
 
   render() {
-    const { localConfig, numberOfTabs } = this.state
+    const {
+      localConfig, numberOfTabs, activeFileName, fileNamesInEditor,
+    } = this.state
     const { width, height } = localConfig
-    const children = <CodeBlock color="#ffffff" />
     return (
       <div>
-        <GetTabs numberOfTabs={numberOfTabs} width={width} />
+        <GetTabs numberOfTabs={numberOfTabs} width={width} fileNamesInEditor={fileNamesInEditor} />
         <Wrapper width={width} height={height}>
-          <IconWrapper>
-            <Icon type={children} />
-          </IconWrapper>
           <E>
+            <ActiveFile>{activeFileName}</ActiveFile>
             <Editor />
           </E>
         </Wrapper>
@@ -105,5 +165,17 @@ class EditorWrapper extends React.PureComponent {
     )
   }
 }
-
+EditorWrapper.propTypes = {
+  activeFileName: PropTypes.string,
+  fileNamesInEditor: PropTypes.shape,
+}
+EditorWrapper.defaultProps = {
+  activeFileName: 'untitled.js',
+  fileNamesInEditor: {
+    firstFile: 'untitled-1.js',
+    secondFile: 'untitled-2.js',
+    thirdFile: 'untitled-3.js',
+    fallback: 'untitled.js',
+  },
+}
 export default EditorWrapper
